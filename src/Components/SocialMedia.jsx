3019df6700dc1da369921e20d4c6b06f8ae96e89
tdark23 <89210225+tdark23@users.noreply.github.com>
2022-03-03 15:20:32 +0100

    Update SocialMedia.jsx

--- src/Components/SocialMedia.jsx
+++ src/Components/SocialMedia.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import { BsTwitter, BsInstagram } from 'react-icons/bs';
+import { BsLinkedin, BsInstagram } from 'react-icons/bs';
 import { FaFacebookF } from 'react-icons/fa';
 
 const SocialMedia = () => (
   <div className="app__social">
-    <div>
-      <BsTwitter />
-    </div>
-    <div>
-      <FaFacebookF />
-    </div>
-    <div>
-      <BsInstagram />
-    </div>
+      <a href='https://www.linkedin.com/in/dark-teddy-a9b456227'>
+        <BsLinkedin />
+      </a>
+      <a href='https://www.facebook.com/darkted'>
+        <FaFacebookF />
+      </a>
+      <a href='https://www.instagram.com/lil30freak'>
+        <BsInstagram />
+      </a>
   </div>
 );
 
